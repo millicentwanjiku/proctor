c9dc3784ff4fe1b63d6ff69c7cc03a1b78d0933a
millicentwanjiku <milicentnjuguna0@gmail.com>
2017-01-14 23:33:12 +0300

    creating withdraw and deposit methods

---
+++ oop/bankaccount.py
@@ -0,0 +1,11 @@
+"""OBJECT ORIENTED LAB"""
+class BankAccount(object):
+	"""Bankaccount class with methods withdraw and deposit"""
+	def __init__(self):
+		pass
+
+	def withdraw():
+		pass
+
+	def deposit():
+		pass
--- oop/testbankaccount.py
+++ oop/testbankaccount.py
@@ -1,4 +1,5 @@
 import unittest
+import bankaccount
 
 class CurrentAccountTestCases(unittest.TestCase):
   def setUp(self):
