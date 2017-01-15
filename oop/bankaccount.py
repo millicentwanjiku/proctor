87a3e589df16389eab1725ba30b39265a0c26561
millicentwanjiku <milicentnjuguna0@gmail.com>
2017-01-15 15:19:37 +0300

    implementing methods withdraw and deposit

--- oop/bankaccount.py
+++ oop/bankaccount.py
@@ -1,11 +1,24 @@
 """OBJECT ORIENTED LAB"""
 class BankAccount(object):
-	"""Bankaccount class with methods withdraw and deposit"""
-	def __init__(self):
-		pass
+    """Bankaccount class with methods withdraw and deposit"""
+    def __init__(self):
+        pass
+    def withdraw():
+        pass
+    def deposit():
+        pass
+class SavingsAccount(BankAccount):
+    """Class saving account that inherits from bank account"""
+    def __init__(self):
+        balance = 500
+    def deposit(self, deposit):
+        if deposit > 0:
+            balance += deposit
+            return balance
+        else:
+            return "Invalid deposit amount"
 
-	def withdraw():
-		pass
+    
 
-	def deposit():
-		pass
+
+
