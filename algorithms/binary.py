7a26d27bb9a03993b8422992fa916bcaa2b97981
millicentwanjiku <milicentnjuguna0@gmail.com>
2017-01-15 21:53:33 +0300

    intial files

--- algorithms/binary.py
+++ algorithms/binary.py
@@ -1,4 +1,8 @@
 """Binary converter"""
 def binary_converter(number):
-	"""A function that returns a binary number"""
-	if number > 0 and number >= 255:
+    """A function that returns a binary number"""
+    if number >= 0 and number <= 255:
+        bina = str(bin(number))
+        return bina[2:]
+    else:
+        return "Invalid input"
